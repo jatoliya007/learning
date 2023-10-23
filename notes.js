13975edc2c3b83a14638abd1e427691a47a7373f
Deepika <regar.deepika7@gmail.com>
2023-10-23 10:36:16 +0530

    adding notes

---
+++ notes.js
@@ -0,0 +1,26 @@
+// let arrey = [2, 4, 8];
+
+// function myfunction(numArr) {
+//     console.log(numArr)
+//     let sum = 0
+//     for (let i = 0; i < numArr.length; i++) {
+//         let elem = numArr[i];
+//         console.log('elem ', elem, i)
+//         sum = sum + elem
+//         console.log('sum ', sum)
+//     }
+//     return sum
+// }
+
+// let result = myfunction(arrey)
+// console.log('result ', result)
+
+
+let arrex = [3, 5, 7, 9];
+function myfunction(paramArr){
+    console.log(paramArr)
+
+    for (let i = 0; i<paramArr.length; i++){
+        
+    }
+}
